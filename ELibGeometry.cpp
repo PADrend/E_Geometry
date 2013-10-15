80a08a72c556bb67375a745228ef3f874278aaa2
Benjamin Eikel <benjamin@eikel.org>
2013-10-15 12:59:11 +0200

    Import E_Geometry from SVN repository r821
    Drop history intentionally

---
+++ ELibGeometry.cpp
@@ -0,0 +1,111 @@
+/*
+	This file is part of the E_Geometry library.
+	Copyright (C) 2007-2012 Benjamin Eikel <benjamin@eikel.org>
+	Copyright (C) 2007-2012 Claudius Jähn <claudius@uni-paderborn.de>
+	Copyright (C) 2007-2012 Ralf Petring <ralf@petring.net>
+	
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the 
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#include "ELibGeometry.h"
+#include "E_Box.h"
+#include "E_Frustum.h"
+#include "E_Line3.h"
+#include "E_Matrix3x3.h"
+#include "E_Matrix4x4.h"
+#include "E_Plane.h"
+#include "E_PointOctree.h"
+#include "E_Quaternion.h"
+#include "E_Ray3.h"
+#include "E_Rect.h"
+#include "E_Segment3.h"
+#include "E_Sphere.h"
+#include "E_SRT.h"
+#include "E_Tetrahedron.h"
+#include "E_Triangle.h"
+#include "E_Vec2.h"
+#include "E_Vec3.h"
+#include "E_Vec4.h"
+#include <EScript/Utils/DeprecatedMacros.h>
+#include <EScript/Basics.h>
+#include <EScript/StdObjects.h>
+#include <Geometry/Definitions.h>
+#include <Geometry/Vec3.h>
+#include <Geometry/Tools.h>
+
+namespace E_Geometry {
+
+void init(EScript::Namespace * globals) {
+	using namespace Geometry;
+	
+	EScript::Namespace * lib = new EScript::Namespace();
+	declareConstant(globals, "Geometry", lib);
+	E_Box::init(*lib);
+	E_Frustum::init(*lib);
+	E_Line3::init(*lib);
+	E_Matrix3x3::init(*lib);
+	E_Matrix4x4::init(*lib);
+	E_Plane::init(*lib);
+	E_PointOctree::init(*lib);
+	E_Quaternion::init(*lib);
+	E_Ray3::init(*lib);
+	E_Rect::init(*lib);
+	E_Segment3::init(*lib);
+	E_Sphere::init(*lib);
+	E_SRT::init(*lib);
+	E_Tetrahedron::init(*lib);
+	E_Triangle::init(*lib);
+	E_Vec2::init(*lib);
+	E_Vec3::init(*lib);
+	E_Vec4::init(*lib);
+
+	declareConstant(lib,"CORNER_xyz", CORNER_xyz);
+	declareConstant(lib,"CORNER_Xyz", CORNER_Xyz);
+	declareConstant(lib,"CORNER_xYz", CORNER_xYz);
+	declareConstant(lib,"CORNER_XYz", CORNER_XYz);
+	declareConstant(lib,"CORNER_xyZ", CORNER_xyZ);
+	declareConstant(lib,"CORNER_XyZ", CORNER_XyZ);
+	declareConstant(lib,"CORNER_xYZ", CORNER_xYZ);
+	declareConstant(lib,"CORNER_XYZ", CORNER_XYZ);
+
+	declareConstant(lib,"SIDE_X_NEG", SIDE_X_NEG);
+	declareConstant(lib,"SIDE_Y_NEG", SIDE_Y_NEG);
+	declareConstant(lib,"SIDE_Z_NEG", SIDE_Z_NEG);
+	declareConstant(lib,"SIDE_X_POS", SIDE_X_POS);
+	declareConstant(lib,"SIDE_Y_POS", SIDE_Y_POS);
+	declareConstant(lib,"SIDE_Z_POS", SIDE_Z_POS);
+
+	//! [ESF] Vec3 getClosestPointOnLineToRay(Vec3 linePos, Vec3 lineDir, Vec3 rayPos, Vec3 rayDir)
+	ES_FUNCTION2(lib,"getClosestPointOnLineToRay",4,4,{
+		const Vec3 & linePos = parameter[0].to<const Vec3&>(rt);
+		const Vec3 & lineDir = parameter[1].to<const Vec3&>(rt);
+		const Vec3 & rayPos = parameter[2].to<const Vec3&>(rt);
+		const Vec3 & rayDir = parameter[3].to<const Vec3&>(rt);
+		return EScript::create(std::move(getClosestPointOnLineToRay(linePos, lineDir, rayPos, rayDir)));
+	})
+
+	//! [ESF] Vec3|false rayPlaneIntersection(Vec3 planePos, Vec3 planeNormal, Vec3 rayPos, Vec3 rayDir)
+	ES_FUNCTION2(lib,"rayPlaneIntersection",4,4,{
+		const Vec3 & planePos = parameter[0].to<const Vec3&>(rt);
+		const Vec3 & planeNormal = parameter[1].to<const Vec3&>(rt);
+		const Vec3 & rayPos = parameter[2].to<const Vec3&>(rt);
+		const Vec3 & rayDir = parameter[3].to<const Vec3&>(rt);
+		Vec3 intersection;
+		bool result = rayPlaneIntersection(planePos, planeNormal, rayPos, rayDir, intersection);
+		if (result) {
+			return EScript::create(std::move(intersection));
+		} else {
+			return EScript::Bool::create(false);
+		}
+	})
+
+	//! [ESF] Frustum calcEnclosingOrthoFrustum(Box, Matrix4x4)
+	ES_FUNCTION2(lib, "calcEnclosingOrthoFrustum", 2, 2, {
+		const Box & box = parameter[0].to<const Box&>(rt);
+		const Matrix4x4f & modelView = parameter[1].to<const Matrix4x4&>(rt);
+		return new E_Frustum(calcEnclosingOrthoFrustum(box, modelView));
+	})
+}
+
+}
---
+++ E_Triangle.cpp
@@ -0,0 +1,76 @@
+/*
+	This file is part of the E_Geometry library.
+	Copyright (C) 2007-2012 Benjamin Eikel <benjamin@eikel.org>
+	Copyright (C) 2007-2012 Claudius Jähn <claudius@uni-paderborn.de>
+	Copyright (C) 2007-2012 Ralf Petring <ralf@petring.net>
+	
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the 
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#include "E_Triangle.h"
+#include "E_Vec3.h"
+#include <EScript/Basics.h>
+#include <EScript/StdObjects.h>
+#include <Geometry/Triangle.h>
+#include <Geometry/Vec3.h>
+#include <sstream>
+
+namespace E_Geometry {
+
+//! (static)
+EScript::Type * E_Triangle::getTypeObject(){
+	static EScript::Type * typeObject = new EScript::Type(EScript::Object::getTypeObject()); // ---|> Object
+	return typeObject;
+}
+
+//!	(static) init members
+void E_Triangle::init(EScript::Namespace & lib) {
+	EScript::Type * typeObject = E_Triangle::getTypeObject();
+	declareConstant(&lib,getClassName(),typeObject);
+
+	using namespace Geometry;
+	
+	//!	[ESMF] line new Triangle(Vec3 a, Vec3 b, Vec3 c)
+	ES_CONSTRUCTOR(typeObject, 3, 3, {
+		return new E_Triangle(Triangle<Vec3f>(
+								parameter[0].to<const Vec3&>(rt),
+								parameter[1].to<const Vec3&>(rt),
+								parameter[2].to<const Vec3&>(rt)));
+	})
+
+	//! [ESF] Number Triangle.calcArea()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcArea", 0, 0,	thisObj->calcArea())
+
+	//! [ESF] Vec3 Triangle.calcNormal()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcNormal", 0, 0,std::move(thisObj->calcNormal()))
+
+	//! [ESF] Vec3 Triangle.calcBarycentricCoordinates(Vec3 p)
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcBarycentricCoordinates", 1, 1,
+			std::move(thisObj->calcBarycentricCoordinates(parameter[0].to<const Vec3&>(rt))))
+
+	//! [ESF] Array Triangle.closestPoint(Vec3 p)
+	ES_MFUNCTION(typeObject,const Triangle<Vec3f>, "closestPoint", 1, 1, {
+			const Vec3f & p = parameter[0].to<const Vec3&>(rt);
+			Vec3f barycentric;
+			Vec3f result = thisObj->closestPoint(p, barycentric);
+
+			EScript::Array * array = EScript::Array::create();
+			array->pushBack(EScript::create(std::move(result)));
+			array->pushBack(EScript::create(std::move(barycentric)));
+			return array;
+	})
+
+	//! [ESF] Number Triangle.distanceSquared(Vec3 p)
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "distanceSquared", 1, 1,
+			thisObj->distanceSquared(parameter[0].to<const Vec3&>(rt)))
+
+}
+
+std::string E_Triangle::toString() const {
+	std::ostringstream stream;
+	stream << getClassName() << '(' << (**this) << ')';
+	return stream.str();
+}
+
+}
