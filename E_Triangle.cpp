582965b0a29bc7ea289295ee3d000a93045a606b
Sascha Brandt <myeti@mail.upb.de>
2017-10-18 17:08:30 +0200

    added getCircumcenter to Triangle

--- E_Triangle.cpp
+++ E_Triangle.cpp
@@ -43,14 +43,14 @@ void E_Triangle::init(EScript::Namespace & lib) {
 	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcArea", 0, 0,	thisObj->calcArea())
 
 	//! [ESF] Vec3 Triangle.calcNormal()
-	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcNormal", 0, 0,std::move(thisObj->calcNormal()))
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcNormal", 0, 0,thisObj->calcNormal())
 
 	//! [ESF] Vec3 Triangle.calcPoint(Number u, Number v)
-	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcPoint", 2, 2,std::move(thisObj->calcPoint(parameter[0].toFloat(),parameter[1].toFloat())))
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcPoint", 2, 2,thisObj->calcPoint(parameter[0].toFloat(),parameter[1].toFloat()))
 
 	//! [ESF] Vec3 Triangle.calcBarycentricCoordinates(Vec3 p)
 	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcBarycentricCoordinates", 1, 1,
-			std::move(thisObj->calcBarycentricCoordinates(parameter[0].to<Vec3>(rt))))
+			thisObj->calcBarycentricCoordinates(parameter[0].to<Vec3>(rt)))
 
 	//! [ESF] Array Triangle.closestPoint(Vec3 p)
 	ES_MFUNCTION(typeObject,const Triangle<Vec3f>, "closestPoint", 1, 1, {
@@ -63,6 +63,10 @@ void E_Triangle::init(EScript::Namespace & lib) {
 			array->pushBack(EScript::create(std::move(barycentric)));
 			return array;
 	})
+
+	//! [ESF] Vec3 Triangle.calcBarycentricCoordinates()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "calcCircumcenter", 0, 0,
+			thisObj->calcCircumcenter())
 
 	//! [ESF] Number Triangle.distanceSquared(Vec3 p)
 	ES_MFUN(typeObject,const Triangle<Vec3f>, "distanceSquared", 1, 1,
@@ -82,6 +86,22 @@ void E_Triangle::init(EScript::Namespace & lib) {
 	ES_MFUN(typeObject,Triangle<Vec3f>, "setVertexB", 1, 1,(thisObj->setVertexB(parameter[0].to<Vec3>(rt)), thisEObj))
 	//! [ESF] Vec3 Triangle.setVertexC()
 	ES_MFUN(typeObject,Triangle<Vec3f>, "setVertexC", 1, 1,(thisObj->setVertexC(parameter[0].to<Vec3>(rt)), thisEObj))
+
+	//! [ESF] Vec3 Triangle.getEdgeAB()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "getEdgeAB", 0, 0,thisObj->getEdgeAB())	
+	//! [ESF] Vec3 Triangle.getEdgeAC()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "getEdgeAC", 0, 0,thisObj->getEdgeAC())
+	//! [ESF] Vec3 Triangle.getEdgeBA()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "getEdgeBA", 0, 0,thisObj->getEdgeBA())	
+	//! [ESF] Vec3 Triangle.getEdgeBC()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "getEdgeBC", 0, 0,thisObj->getEdgeBC())
+	//! [ESF] Vec3 Triangle.getEdgeCA()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "getEdgeCA", 0, 0,thisObj->getEdgeCA())	
+	//! [ESF] Vec3 Triangle.getEdgeCB()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "getEdgeCB", 0, 0,thisObj->getEdgeCB())
+	
+	//! [ESF] Bool Triangle.isDegenerate()
+	ES_MFUN(typeObject,const Triangle<Vec3f>, "isDegenerate", 0, 0,thisObj->isDegenerate())
 }
 
 std::string E_Triangle::toString() const {
